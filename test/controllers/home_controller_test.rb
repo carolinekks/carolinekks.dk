463dcf7a69622bb8aee66ad0e96db0d3c96ddb3c
carkle <racc.6217@tutanota.com>
2025-05-21 21:08:16 -0700

    fixing(?) test again

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get main" do
-    get home_main_url
+    get root_url
     assert_response :success
   end
 end
