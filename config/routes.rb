64f44663617b750e3cb94464dbdea39b076627a4
carkle <racc.6217@tutanota.com>
2025-11-23 02:47:08 -0800

    monitor

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root "home#main"
   get "up" => "rails/health#show", as: :rails_health_check
   get "/changelog_data", to: "changelog#index"
+  get "/2c085e5c6556b7a9", to: proc { [ 200, { "Content-Type" => "text/plain" }, [ "Access Denied" ] ] }
   post "changelog_webhook", to: "changelog#webhook"
 end
